b57b61993d7adf976748fc00b55df5a877278d67
Andrei Lapitski2 <Andrei_Lapitski2@epam.com>
2021-06-17 13:54:40 +0300

    Task 2-1. DI/IoC and unit testing for controllers

--- NorthwindApp/Pages/Categories/Index.cshtml.cs
+++ NorthwindApp/Pages/Categories/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using NorthwindApp.Helpers;
 using NorthwindApp.Interfaces;
@@ -7,7 +8,7 @@ using NorthwindApp.Models;
 
 namespace NorthwindApp.Pages.Categories
 {
-    public class CategoriesModel : PageModel
+    public class IndexModel : PageModel
     {
         private readonly IRepository<Category> _categoryRepository;
         private readonly IList<string> _rowNames;
@@ -15,15 +16,17 @@ namespace NorthwindApp.Pages.Categories
         public IList<Category> Categories { get; set; }
         public IList<string> RowNames => _rowNames ?? PropertyHelper.GetDisplayablePropertyNames(typeof(Category));
 
-        public CategoriesModel(IRepository<Category> categoryRepository)
+        public IndexModel(IRepository<Category> categoryRepository)
         {
             _categoryRepository = categoryRepository;
             _rowNames = PropertyHelper.GetDisplayablePropertyNames(typeof(Category));
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
             Categories = _categoryRepository.GetAll().ToList();
+
+            return Page();
         }
     }
 }
---
+++ XUnitTestProject/Utilities/TestsHelper.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindApp.Models;
+
+namespace Tests.Utilities
+{
+    public static class TestsHelper
+    {
+        public const string DbName = "NorthwindTest";
+
+        public static DbContextOptions<NorthwindContext> GetTestOptions()
+        {
+            return new DbContextOptionsBuilder<NorthwindContext>().UseInMemoryDatabase(DbName).Options;
+        }
+
+        public static void FillInInMemoryNorthwindContextWithTestData()
+        {
+            using NorthwindContext context = new NorthwindContext(GetTestOptions());
+            context.Categories.AddRange(
+                new Category
+                {
+                    CategoryName = "Category-1",
+                    Description = "Description-1"
+                },
+                new Category
+                {
+                    CategoryName = "Category-2",
+                    Description = "Description-2"
+                },
+                new Category
+                {
+                    CategoryName = "Category-3",
+                    Description = "Description-3"
+                });
+            context.SaveChanges();
+        }
+
+        public static void DropTestDatabase()
+        {
+            using NorthwindContext context = new NorthwindContext(GetTestOptions());
+            context.Database.EnsureDeleted();
+        }
+    }
+}
