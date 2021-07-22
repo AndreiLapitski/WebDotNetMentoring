bf171874f5f417563eaed3749eb43c40fc1b3743
Andrei Lapitski2 <Andrei_Lapitski2@epam.com>
2021-07-22 11:26:58 +0300

    Task 3-3. Html Helpers / Html Tags (Tag Helpers)

---
+++ NorthwindApp/TagHelpers/A.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using NorthwindApp.Helpers;
+
+namespace NorthwindApp.TagHelpers
+{
+    public class A : TagHelper
+    {
+        [HtmlAttributeName("northwind-id")]
+        public int CategoryId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (CategoryId > 0)
+            {
+                output.Attributes.SetAttribute(Constants.Href, $"/images/{CategoryId}");
+            }
+        }
+    }
+}
